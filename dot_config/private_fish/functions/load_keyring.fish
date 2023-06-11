90d1f7e87d0094943ae152c08698298b184172fc
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2023-06-11 16:32:09 -0500

    hide a ugly message on login

--- dot_config/private_fish/functions/load_keyring.fish
+++ dot_config/private_fish/functions/load_keyring.fish
@@ -1,6 +1,6 @@
 
 function load_keyring
-	set -l environ (/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
+	set -l environ (/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh 2> /dev/null)
 	
 	for env in $environ
 		set -gx (string split "=" $env)
